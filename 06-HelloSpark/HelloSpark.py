ab32cd95005b73da1e2395f5eba8e8caee542720
piyushtyagi28@hotmail.com <piyushtyagi.1856@gmail.com>
2024-10-26 17:58:00 +0530

    Added Configuration and utils file

--- 06-HelloSpark/HelloSpark.py
+++ 06-HelloSpark/HelloSpark.py
@@ -1,19 +1,23 @@
 from pyspark.sql import *
 
 from lib.logger import Log4j
+from lib.utils import get_spark_app_config
 
 if __name__ == "__main__" :
 
+    conf = get_spark_app_config()
+
     spark = SparkSession \
         .builder \
-        .appName("HelloSpark") \
-        .master("local[3]") \
+        .config(conf=conf) \
         .getOrCreate()
 
     logger = Log4j(spark)
 
     logger.info("Starting HelloSpark")
 
+    # Reading the config file
+    logger.info(spark.sparkContext.getConf().toDebugString())
 
 
     logger.info("Finished HelloSpark")
