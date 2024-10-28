c1c26c40dcf71ae798004f958520950f833c1cce
piyushtyagi28@hotmail.com <piyushtyagi.1856@gmail.com>
2024-10-28 19:49:06 +0530

    Joins, Outer Joins, Shuffle Joins, Broadcast and Bucketing

---
+++ 23-Bucket-Join/Main.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+
+from lib.logger import Log4j
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Bucket Join Demo") \
+        .master("local[3]") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+    df1 = spark.read.json("data/d1/")
+    df2 = spark.read.json("data/d2/")
+    # df1.show()
+    # df2.show()
+    '''
+    spark.sql("CREATE DATABASE IF NOT EXISTS MY_DB")
+    spark.sql("USE MY_DB")
+
+    df1.coalesce(1).write \
+        .bucketBy(3, "id") \
+        .mode("overwrite") \
+        .saveAsTable("MY_DB.flight_data1")
+
+    df2.coalesce(1).write \
+        .bucketBy(3, "id") \
+        .mode("overwrite") \
+        .saveAsTable("MY_DB.flight_data2")
+    '''
+
+    df3 = spark.read.table("MY_DB.flight_data1")
+    df4 = spark.read.table("MY_DB.flight_data2")
+
+    # Disabling broadcast to check if join is happening without shuffle
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+
+    # We will join the data without shuffle sort by bucketing
+    join_expr = df3.id == df4.id
+    join_df = df3.join(df4, join_expr, "inner")
+
+    join_df.collect()
+    input("press a key to stop...")
